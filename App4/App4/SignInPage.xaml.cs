eb7d09e418eb546e52412ac4ae091b6565b379d4
SAM\Ngoc T P Nguyen <samthui7@gmail.com>
2016-10-10 22:42:30 +0700

    Demo SignIn/SignUp View

---
+++ App4/App4/SignInPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace App4
+{
+    public partial class SignInPage : ContentPage
+    {
+        public SignInPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnSignUpButtonClicked(object sender, EventArgs args)
+        {
+            await Navigation.PushAsync(new SignUpPage());
+        }
+    }
+}
